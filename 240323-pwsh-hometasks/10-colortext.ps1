06decb61cc0ce7f87f90d64afe1874b766d88b2a
Azrubael <116581559+Azrubael@users.noreply.github.com>
2025-01-30 10:24:23 +0200

    Update 10-colortext.ps1
    Added the directions to permit of scripts running in PowerShel without admin rights.
    Also corrected an error.

--- 240323-pwsh-hometasks/10-colortext.ps1
+++ 240323-pwsh-hometasks/10-colortext.ps1
@@ -1,6 +1,9 @@
+# To give permission to run PowerShell script on Windows 10 without admin rights, only for the current session:
+# c:\> Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+
 Write-Host "Blue text from file script" -ForegroundColor Blue
 
 $colors = @("green", "blue", "red", "yellow")
-for ($i=0; i -lt $colors.:ength; $i++) {
+for ($i=0; i -lt $colors.:Length; $i++) {
     Write-Host $colors[$i] -ForegroundColor $colors[$i]
 }
