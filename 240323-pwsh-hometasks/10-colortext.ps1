db81fcf9fb54778efdbd16c3f96c17c1c97876e1
Azrubael <116581559+Azrubael@users.noreply.github.com>
2025-03-30 18:05:45 +0300

    Update 10-colortext.ps1
    Added the commentary to run a script with bypass any restrictions.

--- 240323-pwsh-hometasks/10-colortext.ps1
+++ 240323-pwsh-hometasks/10-colortext.ps1
@@ -1,6 +1,8 @@
-# To give permission to run PowerShell script on Windows 10 without admin rights, only for the current session:
-# c:\> Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+<# To give permission to run PowerShell script on Windows 10 without admin rights, only for the current session:
+ # c:\> Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 
+ # OR
+ # c:\> powershell -ExecutionPolicy Bypass -File .\10-colortext.ps1
 Write-Host "Blue text from file script" -ForegroundColor Blue
 
 $colors = @("green", "blue", "red", "yellow")
